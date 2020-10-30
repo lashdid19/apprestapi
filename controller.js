b0a325358edf5d7181b4097f83e2fe8ada4f6ca9
lashdid19 <lashdid19@gmail.com>
2020-10-30 07:58:25 +0700

    create manusia in routes

--- controller.js
+++ controller.js
@@ -6,3 +6,13 @@ var connection = require('./connection');
 exports.index = function(req, res){
     response.ok("Hooray! Your Rest Api is running!", res)
 }
+
+exports.displayManusia = function(req, res){
+    connection.query('SELECT * FROM `manusia`', (error, rows, fileds) => {
+        if(error){
+            console.log(error);
+        }else{
+            response.ok(rows, res);
+        }
+    })
+}
--- routes.js
+++ routes.js
@@ -5,4 +5,7 @@ module.exports = function(app){
 
     app.route('/')
         .get(myjson.index);
+
+    app.route('/manusia')
+        .get(myjson.displayManusia);
 }
